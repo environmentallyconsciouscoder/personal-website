b0d34740ed349129b46ddbcddba6891d2515d269
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-28 02:33:27 +0000

    feat: added text box component

---
+++ src/components/text_box.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface TextBoxProps {
+  children: React.ReactNode;
+  backgroundColor?: string;
+  flexDirection?: 'row' | 'column'; // New prop for flex direction
+}
+
+const StyledDiv = styled.div<{ backgroundColor?: string; flexDirection?: 'row' | 'column' }>`
+  color: rgb(108 40 134 / var(--tw-text-opacity));
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  background-color: ${(props) => props.backgroundColor || 'rgb(244 114 182)'};
+  border-radius: 0.75rem;
+  width: 66.666667%;
+
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection || 'column'}; // Use the prop here
+
+  @media (max-width: 600px) {
+    flex-wrap: wrap;
+    // align-item: center;
+    align-content: center;
+    width: 100%;
+  }
+`;
+
+const TextBox: React.FC<TextBoxProps> = ({ children, backgroundColor, flexDirection }) => {
+  return <StyledDiv backgroundColor={backgroundColor} flexDirection={flexDirection}>{children}</StyledDiv>;
+};
+
+export default TextBox;
