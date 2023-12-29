6c2841050c4a950565b6afa8c8964318cfde752b
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-12-29 01:46:24 +0000

    chore: updated vite config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-website/', // Change this to your repository name
+  base: '/repository-name/', // Change this to your repository name
+  build: {
+    outDir: 'dist', // Adjust this based on your actual output directory
+  },
 });
